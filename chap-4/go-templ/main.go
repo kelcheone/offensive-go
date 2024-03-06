084e3fb6f2541300d2e7729413e504be39bdb343
kelche <kelcheone@gmail.com>
2024-03-06 12:44:44 +0300

    feat: exploring templating in go

---
+++ chap-4/go-templ/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+var x = `
+<html>
+<body>
+Hello {{.}}
+</body>
+</html>
+`
+
+func main() {
+	t, err := template.New("hello").Parse(x)
+	if err != nil {
+		panic(err)
+	}
+
+	t.Execute(os.Stdout, "<script>alert('world') </script>")
+}
--- chap-4/go.mod
+++ chap-4/go.mod
@@ -1,3 +1,8 @@
 module chap-4
 
 go 1.22.0
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/urfave/negroni v1.0.0
+)
